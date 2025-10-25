9ac432e5b322cf23b50f3afc268c5cab99c4fab5
IDGS-903-22001007 <79631@alumnos.utleon.edu.mx>
2025-10-25 01:02:00 -0600

    update main

--- sgt/main.py
+++ sgt/main.py
@@ -1,12 +1,16 @@
+# Archivo: main.py
+from flask import Flask
 
+# Esta es la instancia de la aplicación que Gunicorn necesita encontrar
+# La llamamos 'app'
+app = Flask(__name__)
 
-printl("Hola ");
+# Define una ruta simple para probar la conexión
+@app.route("/")
+def hello_world():
+    # El mensaje final para verificar que la Actividad 15 fue exitosa
+    return "<p>✅ ¡Despliegue CI/CD Exitoso! La Actividad 15 funciona.</p>"
 
- x = 4
-
- y = 3
-
-
- if x in range :
- print("x es mas grande ");
- 
+# Este bloque es solo para ejecutar localmente, Gunicorn lo ignora
+if __name__ == "__main__":
+    app.run(debug=True)
