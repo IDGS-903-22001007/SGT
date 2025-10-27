078725d81a891207c2b284c7205e95aa3cd6d6d7
IDGS-903-22001007 <79631@alumnos.utleon.edu.mx>
2025-10-27 02:29:55 -0600

    Update main.py

--- sgt/main.py
+++ sgt/main.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 # Define una ruta simple para probar la conexión
 @app.route("/")
 def hello_world():
-    # El mensaje final para verificar que la Actividad 15 fue exitosa
-    return "<p>✅ ¡Despliegue CI/CD Exitoso! La Actividad 15 funciona.</p>"
+    # El mensaje final para verificar que la Actividad 15 fue exitosa
+    return "<p>✅ ¡Despliegue CI/CD Exitoso! La Actividad 15 funciona.</p>"
 
 # Este bloque es solo para ejecutar localmente, Gunicorn lo ignora
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)
