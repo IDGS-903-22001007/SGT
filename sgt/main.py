e2780aaa6d6e9777d08f79a1821838adf92fc534
IDGS-903-22001007 <79631@alumnos.utleon.edu.mx>
2025-10-27 01:42:06 -0600

    Refactor Flask app initialization and route
    Removed unused os import and simplified Flask app configuration.

--- sgt/main.py
+++ sgt/main.py
@@ -1,17 +1,9 @@
-import os
 from flask import Flask, render_template
 
-# Obtenemos la ruta absoluta de la carpeta donde reside main.py (que es /sgt en el servidor)
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-# Configuramos la aplicación Flask para que busque las plantillas
-# dentro de la subcarpeta 'templates' que debe estar en el mismo lugar que main.py
-# Por ejemplo: /home/site/wwwroot/sgt/templates
-app = Flask(__name__, template_folder=os.path.join(basedir, 'templates')) 
+app = Flask(__name__)
 
 @app.route('/')
-def index():
-    # Esta función busca login.html dentro de la carpeta 'templates'
+def home():
     return render_template('login.html')
 
 if __name__ == '__main__':
