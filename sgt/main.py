6bead5ef2393b4bfbe64cf52e682e4aef2394050
IDGS-903-22001007 <79631@alumnos.utleon.edu.mx>
2025-10-27 11:58:51 -0600

    Update main.py

--- sgt/main.py
+++ sgt/main.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = "clave-secreta"  # Necesario para los mensajes flash
 
-@app.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # No importa el usuario o contraseña, siempre éxito
+        flash("✅ Inicio de sesión exitoso", "success")
+        return redirect(url_for('login'))
+
     return render_template('login.html')
 
 if __name__ == '__main__':
-    # Solo para pruebas locales
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
