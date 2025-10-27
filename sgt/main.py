349b37402d180ba58ddc5211154df5b3d3fe98f2
IDGS-903-22001007 <79631@alumnos.utleon.edu.mx>
2025-10-27 00:42:36 -0600

    Change login route to render template instead of serving

--- sgt/main.py
+++ sgt/main.py
@@ -1,14 +1,13 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
-# Configuramos la app sin ruta de plantillas para que sirva archivos estáticos
-app = Flask(__name__)
+# Configuramos la app para que busque plantillas en el directorio actual ('.')
+# Esto debe coincidir con la ubicación de login.html
+app = Flask(__name__, template_folder='.') 
 
-# Definimos la ruta de inicio ('/')
 @app.route('/')
 def index():
-    # Usamos send_from_directory para servir el archivo login.html
-    # El punto ('.') es el directorio actual (que será /home/site/wwwroot/sgt después del despliegue)
-    return send_from_directory('.', 'login.html')
+    # Renderizamos el archivo login.html
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
