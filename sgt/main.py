d0f0891eab223bb0639dd412c0ecd120dcc1255d
IDGS-903-22001007 <79631@alumnos.utleon.edu.mx>
2025-10-27 01:16:08 -0600

    Update index route to use render_template

--- sgt/main.py
+++ sgt/main.py
@@ -1,16 +1,12 @@
-from flask import Flask, render_template, send_from_directory
-import os
+from flask import Flask, render_template
 
 # Flask automáticamente busca en la subcarpeta 'templates'
-# Para evitar eso, usamos send_from_directory
 app = Flask(__name__) 
 
 @app.route('/')
 def index():
-    # 🔥 CORRECCIÓN PARA ESTRUCTURA PLANA:
-    # 'os.path.dirname(__file__)' obtiene la ruta de la carpeta actual (/sgt).
-    # 'login.html' es el nombre del archivo a servir.
-    return send_from_directory(os.path.dirname(__file__), 'login.html')
+    # Flask buscará este archivo en la carpeta 'templates'
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
