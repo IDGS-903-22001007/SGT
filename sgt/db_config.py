27f6155e2058a9fd4e66fc9b34d4d4aca639f137
21002096@alumnos.utleon.edu.mx <jr960219@gmail.com>
2025-11-02 23:56:05 -0600

    Fixes AB#6 Configurar base de datos de usuarios (tablas y conexión MySQL)

---
+++ sgt/db_config.py
@@ -0,0 +1,15 @@
+import pyodbc
+
+def get_connection():
+    try:
+        conn = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server};'
+            'SERVER=RANGEL019\\MSSQLSERVER01;'
+            'DATABASE=SGT_DB;'
+            'UID=sa;'
+            'PWD=root;'
+        )
+        return conn
+    except Exception as e:
+        print("Error de conexión a SQL Server:", e)
+        return None
